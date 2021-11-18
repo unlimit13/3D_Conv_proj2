4a86b6a92beabaa51d4f62ad97f17a7f98875044
By_juun <neostgeart@gmail.com>
2021-11-18 12:22:09 +0000

    MultiThread

--- projAVX_M.c
+++ projAVX_M.c
@@ -8,42 +8,46 @@ typedef struct params
   float*** input;
   float*** kernel;
   float*** output;
-  int i,j,k,m;
+  int i,j,k;
   int kernel_size;
 }Params;
 
 void *woker(void *arg){
     Params *data = (Params *)arg;
-    float **input_temp = aligned_alloc(32,sizeof(float*) * (data->kernel_size));
-    float **kernel_temp = aligned_alloc(32,sizeof(float*) * (data->kernel_size));
-    for(int l = 0; l<(data->kernel_size);l++){
-        input_temp[l] = aligned_alloc(32,sizeof(float) * (data->kernel_size));
-        kernel_temp[l] = aligned_alloc(32,sizeof(float) * (data->kernel_size));
-    }
-                     
-    for(int o=0;o<(data->kernel_size);o++){
-        for(int p=0;p<(data->kernel_size);p++){
-            input_temp[o][p] = data->input[data->i+data->m][data->j+o][data->k+p];
-            kernel_temp[o][p] = data->kernel[data->m][o][p];
-                             
-                            //output[i][j][k] += input[i+m][j+o][k+p] * kernel[m][o][p];
+    int kernel_height = data->kernel_size;
+    for(int m=0;m<kernel_height;m++){
+        float **input_temp = aligned_alloc(32,sizeof(float*) * kernel_height);
+        float **kernel_temp = aligned_alloc(32,sizeof(float*) * kernel_height);
+        for(int l = 0; l<kernel_height;l++){
+            input_temp[l] = aligned_alloc(32,sizeof(float) * kernel_height);
+            kernel_temp[l] = aligned_alloc(32,sizeof(float) * kernel_height);
+        }
+                        
+        for(int o=0;o<kernel_height;o++){
+            for(int p=0;p<kernel_height;p++){
+                input_temp[o][p] = data->input[data->i+m][data->j+o][data->k+p];
+                kernel_temp[o][p] = data->kernel[m][o][p];
+                                
+                                //output[i][j][k] += input[i+m][j+o][k+p] * kernel[m][o][p];
+            }
+        }
+        __m256 A[kernel_height],B[kernel_height],result[kernel_height];
+        for(int o=0;o<kernel_height;o++){
+            A[o] = _mm256_load_ps(input_temp[o]);
+            B[o] = _mm256_load_ps(kernel_temp[o]);
+        }
+        float *values[kernel_height];
+        for(int o=0;o<kernel_height;o++){
+            result[o] = _mm256_mul_ps(A[o],B[o]);
+            values[o] = (float *)&result[o]; 
+        }
+        for(int o=0;o<kernel_height;o++){
+            for(int v=0;v<kernel_height;v++){
+                data->output[data->i][data->j][data->k] += values[o][v];
+            }
         }
     }
-    __m256 A[data->kernel_size],B[data->kernel_size],result[data->kernel_size];
-    for(int o=0;o<data->kernel_size;o++){
-        A[o] = _mm256_load_ps(input_temp[o]);
-        B[o] = _mm256_load_ps(kernel_temp[o]);
-    }
-    float *values[data->kernel_size];
-    for(int o=0;o<data->kernel_size;o++){
-        result[o] = _mm256_mul_ps(A[o],B[o]);
-        values[o] = (float *)&result[o]; 
-    }
-    for(int o=0;o<data->kernel_size;o++){
-        for(int v=0;v<data->kernel_size;v++){
-            data->output[data->i][data->j][data->k] += values[o][v];
-        }
-    }
+    
     pthread_exit(NULL);
 }
 
@@ -55,9 +59,9 @@ void Multi_3DConv(float ***input,float ***kernel,float ***output,int row, int co
         for(int j=0;j<col_length;j++){
             for(int k=0;k<row_length;k++){
                 pthread_t *threads;
-                threads = (pthread_t *)malloc(sizeof(pthread_t)*kernel_height);
+                threads = (pthread_t *)malloc(sizeof(pthread_t)*row_length);
                 //printf("here1!\n");
-                for(int m=0;m<kernel_height;m++){
+                
                     Params* thread_params;
                     thread_params = (Params*)malloc(sizeof(Params));
                     //printf("here2!\n");
@@ -67,17 +71,14 @@ void Multi_3DConv(float ***input,float ***kernel,float ***output,int row, int co
                     thread_params->i = i;
                     thread_params->j = j;
                     thread_params->k = k;
-                    thread_params->m = m;
                     thread_params->kernel_size = kernel_height;
                     //printf("here3!\n");
                     //printf("m : %d\n",m);
-                    pthread_create(&threads[m],NULL,woker,(void*)thread_params);
-                    //pthread_detach(threads[m]);
+                    pthread_create(&threads[k],NULL,woker,(void*)thread_params);
+                    pthread_detach(threads[k]);
                     //printf("here4\n");
-                }
-                for(int m=0;m<kernel_height;m++){
-                    pthread_join(threads[m],NULL);
-                }
+                
+               
                 //printf("here5\n");
             }
             
